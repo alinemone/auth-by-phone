6cdc7d9983c01118d95030b10c1b3d47eb29042d
Ali Alimohammadi <alimohammayali@gmail.com>
2020-09-07 12:53:12 +0430

    all project login by phon in laravel 7

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\ActiveCode;
+use App\Http\Controllers\Controller;
+use App\Notifications\ActiveCode as ActiveCodeNotification;
+use App\User;
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    public function showLogin()
+    {
+        return view('auth.login');
+    }
+
+    public function login(Request $request)
+    {
+        $validData = $request->validate([
+            'phone' => ['required' , 'regex:/^09(1[0-9]|3[1-9]|2[1-9])-?[0-9]{3}-?[0-9]{4}$/' ,'exists:users,phone']
+        ]);
+
+        $user = User::wherePhone($validData['phone'])->first();
+
+        $request->session()->flash('auth' , [
+            'user_id' => $user->id,
+            'remember' => $request->has('remember')
+        ]);
+
+        $code = ActiveCode::generateCode($user);
+        $user->notify(new ActiveCodeNotification($code , $user->phone));
+
+        return redirect(route('auth.phone.token'));
+    }
+}
---
+++ app/Http/Controllers/Auth/TokenController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\ActiveCode;
+use App\Http\Controllers\Controller;
+use App\Notifications\LoginToWebsite as LoginToWebsiteNotification;
+use App\User;
+use Illuminate\Http\Request;
+
+class TokenController extends Controller
+{
+    public function showToken(Request $request)
+    {
+        if(! $request->session()->has('auth')) {
+            return redirect(route('auth.login'));
+        }
+
+        $request->session()->reflash();
+
+        return view('auth.token');
+    }
+
+    public function token(Request $request)
+    {
+        $request->validate([
+            'token' => 'required'
+        ]);
+
+        if(! $request->session()->has('auth')) {
+            return redirect(route('auth.login'));
+        }
+
+        $user = User::findOrFail($request->session()->get('auth.user_id'));
+        $status = ActiveCode::verifyCode($request->token , $user);
+
+        if(! $status) {
+//            alert()->error('کد صحیح نبود');
+            return redirect(route('auth.login'));
+        }
+
+        if(auth()->loginUsingId($user->id,$request->session()->get('auth.remember'))) {
+            $user->activeCode()->delete();
+            return redirect('/');
+        }
+
+        return redirect(route('auth.login'));
+    }
+}
